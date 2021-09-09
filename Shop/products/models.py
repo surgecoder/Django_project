afdf750ae756542d2a2e080dfb88cf0c39d9c144
milad jarrah <milad.jarrah@gmail.com>
2021-09-10 04:04:25 +0430

    Adding initial Products models

--- Shop/products/models.py
+++ Shop/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Categories(models.Model):
+    title = models.CharField(max_length=100, required=True)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, required=True)
+    price = models.DecimalField(max_digits=11, decimal_places=0)
+    category = models.ManyToManyField(Categories)
+
+    def __str__(self):
+        return self.title
